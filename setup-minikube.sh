d7334fbd65b99a9547411132b006213da37dcf54
lukas <lukas.engdahl@hotmail.com>
2024-09-06 14:56:43 +0200

    added secret in setup-minikube script takes values from .env file

--- setup-minikube.sh
+++ setup-minikube.sh
@@ -9,6 +9,11 @@ echo "Tillämpar Kubernetes-konfiguration..."
 kubectl apply -f deployment.yaml
 kubectl apply -f postgres-init-configmap.yaml
 
+# Skapa Secret för databasuppgifter
+echo "Skapar Secret för databasens inloggning..."
+kubectl create secret generic db-secret --from-literal=POSTGRES_USER=${POSTGRES_USER} --from-literal=POSTGRES_PASSWORD=${POSTGRES_PASSWORD} --from-literal=POSTGRES_DB=${POSTGRES_DB}
+
+
 # Kontrollera status för resurser
 echo "Kontrollerar status för Pods, Services och Deployments..."
 kubectl get pods 
